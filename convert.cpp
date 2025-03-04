42dd9d33f70ccb5c3404af1249b6be7982553edf
Michael L. Collard <collard@uakron.edu>
2025-03-04 14:58:29 -0500

    Add concerns

--- convert.cpp
+++ convert.cpp
@@ -13,17 +13,17 @@
 int main(int argc, char* argv[]) {
 
     // requires conversion option and string
-    // @concerns
+    // @concerns input_format, argc, argv, error handling, std::cerr
     if (argc != 3) {
         std::cerr << "usage: " << argv[0] << " <option> <string>\n";
         return 1;
     }
 
     // convert the string according to the option
-    // @concerns
-    // @concerns
-    // @concerns
-    // @concerns
+    // @concerns input_format, argc, argv, std::string, text[out]
+    // @concerns std::string_view, "--upper", "--lower"
+    // @concerns std::toupper(), std::tolower(), iteration
+    // @concerns error handling, std::cerr
     std::string text(argv[2]);
     if (std::string_view(argv[1]) == "--upper") {
 
@@ -42,7 +42,7 @@ int main(int argc, char* argv[]) {
     }
 
     // output converted text
-    // @concerns
+    // @concerns output_format, text, std::string, iteration, std::cout
     for (auto pc = text.cbegin(); pc != text.cend(); ++pc)
         std::cout << *pc;
     std::cout << '\n';
