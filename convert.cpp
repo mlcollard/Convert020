1b9d1e7c861b187251144d47fae3f2a0233bc442
Michael L. Collard <collard@uakron.edu>
2025-03-19 13:15:46 -0400

    Add wrappers for conversion to lower and upper

--- convert.cpp
+++ convert.cpp
@@ -9,47 +9,59 @@
 #include <string>
 #include <string_view>
 #include <cctype>
+#include <algorithm>
+
+// convert to upper case
+// @concerns std::toupper(), toUpper[out]
+void toUpper(char& c) {
+    c = std::toupper(c);
+}
+
+// convert to lower case
+// @concerns std::tolower(), toLower[out]
+void toLower(char& c) {
+    c = std::tolower(c);
+}
 
 int main(int argc, char* argv[]) {
 
     // requires conversion option and string
-    // @concerns input_format, argc, argv, error handling, std::cerr
+    // @concerns input format, argc, argv, error handling, std::cerr
     if (argc != 3) {
         std::cerr << "usage: " << argv[0] << " <option> <string>\n";
         return 1;
     }
 
-    // option for the conversion
-    // @concerns input_format, argc, argv, std::string_view. option[out]
+    // chosen option
+    // @concerns input format, argv, std::string_view, option[out]
     std::string_view option(argv[1]);
 
     // text to convert
-    // @concerns input_format, argc, argv, std::string, text[out]
+    // @concerns input format, argv, std::string, text[out]
     std::string text(argv[2]);
 
     // convert the string according to the option
-    // @concerns std::string, text, option
-    // @concerns std::string_view, "--upper", "--lower"
-    // @concerns std::toupper(), std::tolower(), iteration
+    // @concerns "--upper", "--lower"
+    // @concerns toUpper(), toLower(), iteration
     // @concerns error handling, std::cerr
     if (option == "--upper") {
 
         for (auto pc = text.begin(); pc != text.end(); ++pc)
-            *pc = std::toupper(*pc);
+            toUpper(*pc);
 
     } else if (option == "--lower") {
 
         for (auto pc = text.begin(); pc != text.end(); ++pc)
-            *pc = std::tolower(*pc);
+            toLower(*pc);
 
     } else {
 
-        std::cerr << "Invalid conversion option: " << argv[1] << '\n';
+        std::cerr << "Invalid conversion option: " << option << '\n';
         return 1;
     }
 
     // output converted text
-    // @concerns output_format, text, std::string, iteration, std::cout
+    // @concerns output format, text, std::string, iteration, std::cout
     for (auto pc = text.cbegin(); pc != text.cend(); ++pc)
         std::cout << *pc;
     std::cout << '\n';
